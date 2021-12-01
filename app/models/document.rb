9250c9ba9ec94dd40270400760b9f328cffb61bd
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-12-01 06:45:29 +0330

    updated: user model

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,3 +1,10 @@
 class Document < ApplicationRecord
+  before_validation :set_uuid, on: :create
+  validates :id, presence: true, uniqueness: true
   has_rich_text :content
+  belongs_to :user
+
+  def set_uuid
+    self.id = SecureRandom.uuid
+  end
 end
