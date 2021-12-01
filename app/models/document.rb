9a3d381cbd1e41d21f7040bf5ba7b40901045437
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-12-01 10:49:10 +0330

    randomizing models got better

--- app/models/document.rb
+++ app/models/document.rb
@@ -5,6 +5,6 @@ class Document < ApplicationRecord
   belongs_to :user
 
   def set_uuid
-    self.id = SecureRandom.rand(1000000..9999999)  
+    self.id = SecureRandom.rand(10000000..99999999)  
   end
 end
