04ca574f4dfadd5ff829a3297d94610fb1145dff
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-12-02 12:11:57 +0330

    some development changes - not important

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "fa85-37-98-79-180.ngrok.io"
 end
