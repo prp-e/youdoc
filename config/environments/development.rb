f17107204b0bcbd569005654cbcd72355b91cb0b
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-12-02 14:31:57 +0330

    working on confirmation emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,5 +74,14 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.hosts << "089a-37-98-79-180.ngrok.io"
+  config.action_mailer.default_url_options = { :host => 'gmail.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => 'YOUR_EMAIL_ADDRESS',  
+    :password             => 'YOUR_EMAIL_PASSWORD',
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
 end
