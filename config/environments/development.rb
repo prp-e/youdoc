ad4127f09d411baa97f8f07bb8970cb6a356b106
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-12-02 18:21:41 +0330

    Email was successful

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,14 +74,14 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address              => "mail.ganjnamebox.ir",
+    :address              => "smtp.gmail.com",
     :port                 => 587,
     :user_name            => ENV['EMAIL_ADDRESS'],  
     :password             => ENV['EMAIL_PASSWORD'],
-    :authentication       => "plain",
-    :enable_starttls_auto => true
+    :authentication       => "login"
   }
 end
