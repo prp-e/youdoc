bcd539a33ff8d457744ae497fd44430f796ab641
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-12-02 18:59:28 +0330

    Let's back to ganjnamebox.ir

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
+    :address              => "mail.ganjnameboxir",
     :port                 => 587,
     :user_name            => ENV['EMAIL_ADDRESS'],  
     :password             => ENV['EMAIL_PASSWORD'],
