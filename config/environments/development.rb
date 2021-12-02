c202551601ba4ad3e463a70c993b172c2e58d1ab
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-12-02 18:07:24 +0330

    Mail server credentials updated

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
+    :address              => "mail.ganjnamebox.ir",
     :port                 => 587,
     :user_name            => ENV['EMAIL_ADDRESS'],  
     :password             => ENV['EMAIL_PASSWORD'],
