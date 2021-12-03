4d59397bf78e5f710a602ff0530c92686f9cc15b
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-12-03 12:40:28 +0330

    Mail server is now working

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,12 +76,14 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.raise_delivery_errors = true 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address              => "mail.ganjnameboxir",
-    :port                 => 25,
+    :openssl_verify_mode => OpenSSL::SSL::VERIFY_NONE, 
+    :address              => "148.251.200.145",
+    :port                 => 587,
     :user_name            => ENV['EMAIL_ADDRESS'],  
     :password             => ENV['EMAIL_PASSWORD'],
-    :authentication       => "login"
+    :authentication       => "plain"
   }
 end
