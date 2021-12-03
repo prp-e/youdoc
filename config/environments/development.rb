11256625923243aeee380a4817c9c388f3a7a7bc
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-12-03 12:24:07 +0330

    Let's change the f-ing port

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "mail.ganjnameboxir",
-    :port                 => 587,
+    :port                 => 25,
     :user_name            => ENV['EMAIL_ADDRESS'],  
     :password             => ENV['EMAIL_PASSWORD'],
     :authentication       => "login"
